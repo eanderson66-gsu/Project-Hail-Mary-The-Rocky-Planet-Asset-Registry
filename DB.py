887cc39a5988e8ae61c945a675340d16d84c44d3
eanderson66-gsu <eanderson66@student.gsu.edu>
2026-04-07 10:20:58 -0400

    Add initial DB.py file with library imports

---
+++ DB.py
@@ -0,0 +1,7 @@
+#==============================================
+#STEP 0: Import necessary libraries
+#==============================================
+
+import sqlite3
+import pandas as pd
+from Project_Petrova_Code import df_cleaned
